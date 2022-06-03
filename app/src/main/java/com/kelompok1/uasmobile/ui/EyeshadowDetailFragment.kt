fac29222ea0134d549a8831f195455e95a8d227d
royaleaulia <2010817210023@mhs.ulm.ac.id>
2022-06-03 22:47:16 +0800

    Menambahkan file file Eyeshadow

--- app/src/main/java/com/kelompok1/uasmobile/ui/EyeshadowDetailFragment.kt
+++ app/src/main/java/com/kelompok1/uasmobile/ui/EyeshadowDetailFragment.kt
@@ -1,4 +1,24 @@
 package com.kelompok1.uasmobile.ui
 
-class EyeshadowDetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.kelompok1.uasmobile.databinding.FragmentEyeshadowDetailBinding
+
+class EyeshadowDetailFragment : Fragment() {
+    private val viewModel: MakeUpViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentEyeshadowDetailBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        return binding.root
+    }
 }
